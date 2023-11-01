82410ad700c678ac00618e92ae45afe1a7ab2270
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-11-01 02:48:55 -0300

    agregar ejemplo de facturacion

---
+++ assets/data/invoice.object.js
@@ -0,0 +1,24 @@
+const compra = {
+    "number": "A-568/TIA-ITU-2023",
+    "seller": ObjectId("65395ff6b9fd168811f87a1f"),
+    "customer": ObjectId("6539611c0465d5bbfe573a67"),
+    "date": new Date(),
+    "saleDetail": [ {   
+        "product": {
+            "postId": 4,
+            "postTitle": "Samsung A34 256Gb con caja y accesorios",
+            "description": "El telefono ideal para tu rutina, con 256Gb de almacenamiento, 8Gb de RAM, 4 camaras y una bateria de 5000mAh, no te quedes sin el tuyo",
+            "product": "smartphone",
+            "categories": [ "tecnologia", "informatica" ],
+            "user": "Samantha",
+            "stock": 2,
+            "price": 289999.99,
+            "date": ISODate("2023-10-23T20:05:08.735Z"),
+            "postDate": ISODate("2023-10-25T19:57:13.111Z")
+        },
+        "quantity": 2,
+        "price": 579999.98
+    } ],
+    "total": 579999.98,
+    "status": 'entregado'
+}
